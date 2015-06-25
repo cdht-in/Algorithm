f5ccaad4023a959ad5f9b0802c431ddb578f9f3a
cong hui <clinth0107@gmail.com>
2015-06-25 18:28:44 -0400

    fun

---
+++ ArraySummaryRanges.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+/*
+	
+Given a sorted integer array without duplicates, return the summary of its ranges.
+
+For example, given [0,1,2,4,5,7], return ["0->2","4->5","7"].
+	
+*/
+
+vector<string> summaryRanges(vector<int>& nums) 
+{
+        
+}
+
+int main()
+{
+	
+}
