b167815e8e32773e026238e7f801a6090b5b8e53
cong hui <clinth0107@gmail.com>
2015-07-15 10:05:34 -0400

    fun

---
+++ LinkedlistDeleteNodeInALinkedList.cpp
@@ -0,0 +1,31 @@
+/*
+	
+Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+
+Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should become 1 -> 2 -> 4 after calling your function.
+
+*/
+
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(0) {}
+};
+ 
+void deleteNode(ListNode* node) 
+{
+	if(node == 0 || node->next == 0)
+		return;
+	int tmp = node->next->val;
+	node->next->val = node->val;
+	node->val = tmp;
+	
+	ListNode* tmpNode = node->next;
+	node->next = node->next->next;
+	delete tmpNode;
+}
+
+int main()
+{
+	
+}
